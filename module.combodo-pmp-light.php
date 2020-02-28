7193a517b7a7860b51c2ae300de8e29e14f2a94d
acognet <anne-catherine.cognet@combodo.com>
2020-02-28 10:53:08 +0100

    PMP - first version

--- module.combodo-pmp-light.php
+++ module.combodo-pmp-light.php
@@ -17,6 +17,7 @@ SetupWebPage::AddModule(
 		'dependencies' => array(
 			'itop-tickets/2.0.0',
 			'itop-config-mgmt/2.0.0',
+			'combodo-gantt-view/1.0.0',
 
 		),
 		'mandatory' => false,
@@ -55,43 +56,43 @@ SetupWebPage::AddModule(
 
 if (!class_exists('PMPLightInstaller'))
 {
-    // Module installation handler
-    //
-    class PMPLightInstaller extends ModuleInstallerAPI
-    {
-        /**
-         * @inheritdoc
-         */
-        public static function BeforeWritingConfig(Config $oConfiguration)
-        {
-        	// Rule to add to the object copier configuration
-            $aNewRule = array(
-              'source_scope' => 'SELECT Project WHERE status NOT IN (\'close\',\'monitor\')',
-      		    'allowed_profiles' => 'Project Manager,Administrator',
-      		    'menu_label' => 'Class:Project/CreateDeliverable',
-      		    'menu_label/FR FR' => 'Créer un délivrable...',
-      		    'form_label' => 'Create a deliverable from %1$s',
-      		    'form_label/FR FR' => 'Créer un délivrable pour %1$s',
-      		    'report_label' => 'Created from %1$s',
-      		    'report_label/FR FR' => 'Créée depuis %1$s',
-      		    'dest_class' => 'WBS',
-      		    'preset' => 
-      		    array (
-      		      0 => 'copy(id,project_id)',
-      		      1 => 'set(start_date,$current_date$)',
-      		    ),
-      		    'retrofit' => 
-      		    array (
-      		    ),
-            );
+	// Module installation handler
+	//
+	class PMPLightInstaller extends ModuleInstallerAPI
+	{
+		/**
+		 * @inheritdoc
+		 */
+		public static function BeforeWritingConfig(Config $oConfiguration)
+		{
+			// Rule to add to the object copier configuration
+			$aNewRule = array(
+				'source_scope' => 'SELECT Project WHERE status NOT IN (\'close\',\'monitor\')',
+				'allowed_profiles' => 'Project Manager,Administrator',
+				'menu_label' => 'Class:Project/CreateDeliverable',
+				'menu_label/FR FR' => 'Créer un délivrable...',
+				'form_label' => 'Create a deliverable from %1$s',
+				'form_label/FR FR' => 'Créer un délivrable pour %1$s',
+				'report_label' => 'Created from %1$s',
+				'report_label/FR FR' => 'Créée depuis %1$s',
+				'dest_class' => 'WBS',
+				'preset' =>
+					array (
+						0 => 'copy(id,project_id)',
+						1 => 'set(start_date,$current_date$)',
+					),
+				'retrofit' =>
+					array (
+					),
+			);
 
-            // Retrieving object copier rules from conf parameters
+			// Retrieving object copier rules from conf parameters
 			// Note: We don't do anything if object copier is not installed, otherwise its configuration will be set when installed.
-            $aExistingRules = $oConfiguration->GetModuleSetting('itop-object-copier', 'rules', array());
-            if(!empty($aExistingRules))
-            {
-            	$bFound = false;
-            	foreach($aExistingRules as $aExistingRule)
+			$aExistingRules = $oConfiguration->GetModuleSetting('itop-object-copier', 'rules', array());
+			if(!empty($aExistingRules))
+			{
+				$bFound = false;
+				foreach($aExistingRules as $aExistingRule)
 				{
 					if(isset($aExistingRule['menu_label']) && ($aExistingRule['menu_label'] === $aNewRule['menu_label']) )
 					{
@@ -104,11 +105,11 @@ if (!class_exists('PMPLightInstaller'))
 				if($bFound === false)
 				{
 					$aExistingRules[] = $aNewRule;
-                    $oConfiguration->SetModuleSetting('itop-object-copier', 'rules', $aExistingRules);
+					$oConfiguration->SetModuleSetting('itop-object-copier', 'rules', $aExistingRules);
 				}
-            }
+			}
 
-            return $oConfiguration;
-        }
-    }
+			return $oConfiguration;
+		}
+	}
 }
