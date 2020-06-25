a3a0f8d69672baa8e631eeb1b79cf42aa932a294
acognet <anne-catherine.cognet@combodo.com>
2020-06-25 18:22:22 +0200

    version 1.0.0 to 1.0.1

--- module.combodo-pmp-light.php
+++ module.combodo-pmp-light.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-pmp-light/1.0.0',
+	'combodo-pmp-light/1.0.1',
 	array(
 		// Identification
 		//
